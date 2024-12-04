d30b66c823b1455a01dd827f7abb0497300300e5
iKingsman <vladislavkingsman@gmail.com>
2024-12-04 10:14:53 +0300

    feat: add solutions for 3 functions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -585,8 +585,9 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  const random = min + Math.random() * (max + 1 - min);
+  return Math.floor(random);
 }
 
 /**
@@ -599,8 +600,8 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -616,8 +617,12 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let countOddNumbers = 0;
+  for (let i = 1; i <= Math.abs(number); i += 1) {
+    if (i % 2 !== 0) countOddNumbers += 1;
+  }
+  return countOddNumbers;
 }
 
 module.exports = {
